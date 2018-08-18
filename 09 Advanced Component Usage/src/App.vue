6471cb73689b08dbe4d81fdd08cbd1e575f15953
doraeball22 <anurak_b@cmu.ac.th>
2018-08-18 18:51:53 +0700

    send data to other component with props

--- 09 Advanced Component Usage/src/App.vue
+++ 09 Advanced Component Usage/src/App.vue
@@ -2,16 +2,19 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               
+                <!-- ส่งค่าที่ชื่อ quote ไปให้่ Quete component -->
+               <app-quote qoute="A wonderful qoute!"></app-quote>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-
+    import Quote from './components/Quote.vue'
     export default {
-        
+        components: {
+            'app-quote': Quote 
+        }
     }
 </script>
 
